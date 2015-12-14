6dc3dfa4dace112a2d9a716e07b0e8a05cdd756f
OMironov <Mironov.oleg94@gmail.com>
2015-12-15 09:52:14 +1000

    Initial Commit

---
+++ MedOffice/MedOffice/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MedOffice
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
