4263a5872c7235bb1b6b6919df033aec2b3dd13d
OMironov <Mironov.oleg94@gmail.com>
2015-12-15 15:32:14 +1000

    ASPNET Identity added first admin

---
+++ MedOffice/MedOffice/Models/AppDbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using MedOffice.DAL;
+
+namespace MedOffice.Models
+{
+    public class AppDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            var role1 = new IdentityRole { Name = "admin" };
+            var role2 = new IdentityRole { Name = "Doctor" };
+
+            roleManager.Create(role1);
+            roleManager.Create(role2);
+
+            var admin = new ApplicationUser { Email = "SuperAdmin@gmail.com", UserName = "SuperAdmin@gmail.com" };
+            string password = "qwerty123";
+            var result = userManager.Create(admin, password);
+
+            if (result.Succeeded)
+            {
+                userManager.AddToRole(admin.Id, role1.Name);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
